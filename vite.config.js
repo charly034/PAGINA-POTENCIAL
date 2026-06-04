fd9c2c8e7704c3969ad0e7cdc3763282854be876
charly034 <charly034mz@gmail.com>
2026-06-04 12:51:15 -0300

    arreglo favicon

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  const isVercel = process.env.VERCEL === "1";
-
-  return {
-    base: isVercel ? "/" : "/PAGINA-POTENCIAL/",
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
 });
